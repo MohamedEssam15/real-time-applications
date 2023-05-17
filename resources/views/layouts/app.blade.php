bd52cb9e8e9f528d534beb3154bb4335a944dc49
Mohamed Essam <mesam5081@gmail.com>
2023-05-17 19:45:44 +0300

    edit layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,11 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    @stack('styles')
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @stack('scripts')
+    @vite('resources/sass/app.scss')
 </head>
 <body>
     <div id="app">
@@ -76,5 +78,6 @@
             @yield('content')
         </main>
     </div>
+    @vite('resources/js/app.js')
 </body>
 </html>
