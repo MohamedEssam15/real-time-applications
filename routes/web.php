97c714b4737efe9edd3bd134d9e469b49d493159
MohamedEssam15 <mesam5081@gmail.com>
2023-07-24 19:55:43 +0300

    end the game

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,6 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::view('/users','users.showAll')->name('users.all');
 Route::view('/game','game.show')->name('game.show');
+Route::get('/chat',[App\Http\Controllers\ChatController::class,'showChat'])->name('chat.show');
+Route::post('/chat/message',[App\Http\Controllers\ChatController::class,'messageReceived'])->name('chat.message');
+Route::post('/chat/greet/{user}',[App\Http\Controllers\ChatController::class,'greetReceived'])->name('chat.greet');
